9a18302d69f5918d18e711e4b33ace270abee08f
Samidi Jayawickrama <107723505+samidisjg@users.noreply.github.com>
2025-01-06 19:58:57 +0530

    train key point detection with pytorch

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLO
 
-model = YOLO('models/yolo5_last.pt')
+model = YOLO('yolov5n.pt')
 
 # Image
-result = model.predict('D:/projects done/SpinSense-/input_videos/input_video.mp4',conf=0.2,save=True)
-print(result)
-print("boxes:")
-for box in result[0].boxes:
-    print(box)
+result = model.track('D:/projects done/SpinSense-/input_videos/input_video.mp4',conf=0.2,save=True)
+# print(result)
+# print("boxes:")
+# for box in result[0].boxes:
+#     print(box)
