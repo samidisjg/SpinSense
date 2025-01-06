bc8a389ba0ea74cc93cb65b8e39d7c321b93c632
Samidi Jayawickrama <107723505+samidisjg@users.noreply.github.com>
2025-01-06 13:16:57 +0530

    tennisball tracking

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov5n.pt')
+model = YOLO('models/yolo5_last.pt')
 
 # Image
-result = model.predict('D:/projects done/SpinSense-/input_videos/input_video.mp4',save=True)
+result = model.predict('D:/projects done/SpinSense-/input_videos/input_video.mp4',conf=0.2,save=True)
 print(result)
 print("boxes:")
 for box in result[0].boxes:
